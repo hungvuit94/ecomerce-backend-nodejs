16962d45cbdf4090c3a87152fa1cf79ef9cea43f
Hung Vu <hungvu.it.94@gmail.com>
2023-12-22 14:22:42 +0700

    section_1

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const { default: helmet } = require('helmet');
+const compression = require('compression');
+const app = express();
+
+// init middleware
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+// init db
+
+// init router
+app.get('/', (req, res, next) => {
+  const strCompress = 'hello Hungvu';
+  return res.status(200).json({
+    message: 'done',
+    metadata: strCompress.repeat(100000),
+  });
+});
+
+// handle error
+
+module.exports = app;
